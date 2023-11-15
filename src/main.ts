c03f43e6b34e75010d73949785a9b50074c2984e
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2023-11-15 13:46:32 +0530

    update files

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import "./style.css";
+import { renderStats } from "./stats";
+import { renderAnalysis } from "./analysis";
+
+const inputEle = document.querySelector<HTMLTextAreaElement>("#app textarea")!;
+
+const renderResults = () => {
+  const input = inputEle.value;
+
+  renderStats(input);
+  renderAnalysis(input);
+};
+
+inputEle.addEventListener("input", renderResults);
+
+window.addEventListener("load", renderResults);
